dd81b13234fb0d0c09b78c3f626268f58975c233
Joakim_VDL <joakimvandooren@gmail.com>
2025-05-31 04:29:27 +0200

    possible speedups

--- functions.hpp
+++ functions.hpp
@@ -8,6 +8,13 @@
 #include <iomanip>
 #include <cmath>
 
+// For restrict keyword portability
+#if defined(__GNUC__) || defined(__clang__)
+    #define RESTRICT __restrict__
+#else
+    #define RESTRICT
+#endif
+
 //? ----------------------------------------------------------
 //?
 //? Description:
@@ -20,51 +27,36 @@
 
 //! Return double !//
 
-double l1norm_Cpp(double* vector, size_t N)
+double l1norm_Cpp(const double* RESTRICT vector, size_t N)
 {
-
     double norm = 0.0;
-
-    #pragma omp parallel for reduction(+:norm)
-    for (int ii = 0; ii < N; ii++)
+#pragma omp parallel for reduction(+:norm)
+    for (int ii = 0; ii < static_cast<int>(N); ii++)
     {
-        norm = norm + abs(vector[ii]);
+        norm += std::abs(vector[ii]);
     }
-
     return norm;
 }
 
-double l2norm_Cpp(double* vector, size_t N)
+double l2norm_Cpp(const double* RESTRICT vector, size_t N)
 {
-
     double norm = 0.0;
-
-    #pragma omp parallel for reduction(+:norm)
-    for (int ii = 0; ii < N; ii++)
+#pragma omp parallel for reduction(+:norm)
+    for (int ii = 0; ii < static_cast<int>(N); ii++)
     {
-        norm = norm + (vector[ii] * vector[ii]);
+        norm += vector[ii] * vector[ii];
     }
-
-    return sqrt(norm);
+    return std::sqrt(norm);
 }
 
 double factorial(int number)
 {
     double fact = 1.0;
-
-    if (number == 0)
+    int abs_number = std::abs(number);
+    for(int ii = 1; ii <= abs_number; ii++)
     {
-        fact = 1.0;
+        fact *= ii;
     }
-
-    else
-    {
-        for(int ii = 1; ii <= abs(number); ii++)
-        {    
-            fact = fact*ii;    
-        }
-    }
-
     return fact;
 }
 
@@ -73,100 +65,98 @@ double factorial(int number)
 //! Return double* !//
 
 //? ones(y) = (y[0:N] =) 1.0
-void ones_Cpp(double *x, size_t N)                    
+void ones_Cpp(double* RESTRICT x, size_t N)
 {
-    #pragma omp parallel for
-    for (int ii = 0; ii < N; ii++)
+#pragma omp parallel for
+    for (int ii = 0; ii < static_cast<int>(N); ii++)
     {
         x[ii] = 1.0;
     }
 }
 
-//? ones(y) = (y[0:N] =) 1.0
-void eigen_ones_Cpp(double *x, size_t N)                    
+//? eigen_ones(y): y[:] = 0, y[0] = 1
+void eigen_ones_Cpp(double* RESTRICT x, size_t N)
 {
-    #pragma omp parallel for
-    for (int ii = 0; ii < N; ii++)
+#pragma omp parallel for
+    for (int ii = 0; ii < static_cast<int>(N); ii++)
     {
         x[ii] = 0.0;
     }
-    
-    x[0] = 1.0;
+    if (N > 0)
+        x[0] = 1.0;
 }
 
 //? y = x
-void copy_Cpp(double *x, double *y, size_t N)                    
+void copy_Cpp(const double* RESTRICT x, double* RESTRICT y, size_t N)
 {
-    #pragma omp parallel for
-    for (int ii = 0; ii < N; ii++)
+#pragma omp parallel for
+    for (int ii = 0; ii < static_cast<int>(N); ii++)
     {
         y[ii] = x[ii];
     }
 }
 
 //? y = ax
-void axpby_Cpp(double a, double *x, 
-                            double *y, size_t N)                    
+void axpby_Cpp(double a, const double* RESTRICT x, double* RESTRICT y, size_t N)
 {
-    #pragma omp parallel for
-    for (int ii = 0; ii < N; ii++)
+#pragma omp parallel for
+    for (int ii = 0; ii < static_cast<int>(N); ii++)
     {
-        y[ii] = (a * x[ii]);
+        y[ii] = a * x[ii];
     }
 }
 
 //? z = ax + by
-void axpby_Cpp(double a, double *x, 
-                double b, double *y, 
-                            double *z, size_t N)
+void axpby_Cpp(double a, const double* RESTRICT x,
+               double b, const double* RESTRICT y,
+               double* RESTRICT z, size_t N)
 {
-    #pragma omp parallel for
-    for (int ii = 0; ii < N; ii++)
+#pragma omp parallel for
+    for (int ii = 0; ii < static_cast<int>(N); ii++)
     {
-        z[ii] = (a * x[ii]) + (b * y[ii]);
+        z[ii] = a * x[ii] + b * y[ii];
     }
-
 }
 
 //? w = ax + by + cz
-void axpby_Cpp(double a, double *x,
-                double b, double *y,
-                double c, double *z, 
-                            double *w, size_t N)
+void axpby_Cpp(double a, const double* RESTRICT x,
+               double b, const double* RESTRICT y,
+               double c, const double* RESTRICT z,
+               double* RESTRICT w, size_t N)
 {
-    #pragma omp parallel for
-    for (int ii = 0; ii < N; ii++)
+#pragma omp parallel for
+    for (int ii = 0; ii < static_cast<int>(N); ii++)
     {
-        w[ii] = (a * x[ii]) + (b * y[ii]) + (c * z[ii]);
+        w[ii] = a * x[ii] + b * y[ii] + c * z[ii];
     }
 }
 
 //? v = ax + by + cz + dw
-void axpby_Cpp(double a, double *x,
-                double b, double *y,
-                double c, double *z,
-                double d, double *w,
-                            double *v, size_t N)
+void axpby_Cpp(double a, const double* RESTRICT x,
+               double b, const double* RESTRICT y,
+               double c, const double* RESTRICT z,
+               double d, const double* RESTRICT w,
+               double* RESTRICT v, size_t N)
 {
-    #pragma omp parallel for
-    for (int ii = 0; ii < N; ii++)
+#pragma omp parallel for
+    for (int ii = 0; ii < static_cast<int>(N); ii++)
     {
-        v[ii] = (a * x[ii]) + (b * y[ii]) + (c * z[ii]) + (d * w[ii]);
+        v[ii] = a * x[ii] + b * y[ii] + c * z[ii] + d * w[ii];
     }
 }
 
 //? u = ax + by + cz + dw + ev
-void axpby_Cpp(double a, double *x,
-                double b, double *y,
-                double c, double *z,
-                double d, double *w,
-                double e, double *v, 
-                          double *u, size_t N)
+void axpby_Cpp(double a, const double* RESTRICT x,
+               double b, const double* RESTRICT y,
+               double c, const double* RESTRICT z,
+               double d, const double* RESTRICT w,
+               double e, const double* RESTRICT v,
+               double* RESTRICT u, size_t N)
 {
-    #pragma omp parallel for
-    for (int ii = 0; ii < N; ii++)
+#pragma omp parallel for
+    for (int ii = 0; ii < static_cast<int>(N); ii++)
     {
-        u[ii] = (a * x[ii]) + (b * y[ii]) + (c * z[ii]) + (d * w[ii]) + (e * v[ii]);
+        u[ii] = a * x[ii] + b * y[ii] + c * z[ii] + d * w[ii] + e * v[ii];
     }
 }
 
--- main.cpp
+++ main.cpp
@@ -349,7 +349,7 @@ int main(int argc, char** argv)
     cout << "Number of OpenMP threads   : " << num_threads << endl;
     cout << "==================================================" << endl << endl;
 
-    cout << "Done!" << endl;
+    cout << "Simulations complete!" << endl;
 
     return 0;
 }
